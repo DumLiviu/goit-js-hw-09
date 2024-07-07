90d280a7e0e4995d782d7ce540f0f688ce0ebe91
DumLiviu <158556659+DumLiviu@users.noreply.github.com>
2024-07-07 11:42:12 +0300

    Revizuire tema
    Adaugare notiflix

--- js/03-promises.js
+++ js/03-promises.js
@@ -32,12 +32,12 @@ document
 
       createPromise(i + 1, currentDelay)
         .then(({ position, delay }) => {
-          console.log(
+          Notiflix.Notify.success(
             `✅ Fulfilled promise ${position} in ${delay}ms`
           );
         })
         .catch(({ position, delay }) => {
-          console.log(
+          Notiflix.Notify.failure(
             `❌ Rejected promise ${position} in ${delay}ms`
           );
         });
